d55d080e683075d37bbb138639a5e21659cf128e
Enes Kahraman <official.phoique@gmail.com>
2019-03-15 00:35:47 +0300

    Chat ui finish

--- routes/chat.js
+++ routes/chat.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const chatRoute = express.Router();
 
+chatRoute.get('/', (request, response) => {
+  response.render('chat');
+});
+
 // get username
 chatRoute.post('/', (request, response) => {
   response.send(request.body);
