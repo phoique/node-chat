2e2264aa17f4dcd3696e7760694d2467b426c3f1
Enes Kahraman <official.phoique@gmail.com>
2019-03-15 01:22:23 +0300

    user login redirect "/" =>  "/chat"

--- routes/chat.js
+++ routes/chat.js
@@ -5,9 +5,6 @@ chatRoute.get('/', (request, response) => {
   response.render('chat');
 });
 
-// get username
-chatRoute.post('/', (request, response) => {
-  response.send(request.body);
-});
+
 
 module.exports = chatRoute;
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ indexRoute.get('/', (request, response) => {
   response.render('index');
 });
 
+// user login and chat redirect
+indexRoute.post('/', (request, response) => {
+  const { username } = request.body;
+
+  // username != null
+  if(username) {
+    response.redirect('/chat');
+  }
+  else {
+    response.send();
+  }
+
+
+});
+
 module.exports = indexRoute;
