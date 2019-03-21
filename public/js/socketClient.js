9230ce13e18ed88dae790a7359fbfae5ba7e9c62
Enes Kahraman <official.phoique@gmail.com>
2019-03-22 02:41:37 +0300

    socket message list

--- public/js/socketClient.js
+++ public/js/socketClient.js
@@ -1,5 +1,24 @@
 const socket = io();
 
-socket.emit('hello');
+socket.on('messages', (data) => {
 
-socket.on('hi', () => console.log('Client Hi.'));
+  fake = data.fakeMessages;
+  fake.map(messages => {
+
+    // message html p tag
+    let message_p = document.createElement('p');
+    message_p.className = 'tag is-medium is-warning';
+    message_p.appendChild(document.createTextNode(`${messages.username}: ${messages.message}`));
+
+    // message posision div
+    let pos_div = document.createElement('div');
+    pos_div.className = 'message-left';
+    pos_div.appendChild(message_p);
+
+    // Message box append
+    element_id = document.getElementById("message-box");
+    element_id.appendChild(pos_div);
+
+  });
+  
+});
--- socket/socketServer.js
+++ socket/socketServer.js
@@ -1,14 +1,39 @@
 const socketio = require('socket.io');
 const io = socketio();
 
+const fakeMessages = [
+  {
+    id: 1,
+    username: "admin",
+    message: "Hello World."
+  },
+  {
+    id: 2,
+    username: "admin",
+    message: "Hi!"
+  },
+  {
+    id: 3,
+    username: "user",
+    message: "Hi admin!"
+  },
+  {
+    id: 4,
+    username: "name",
+    message: "Hi Man!"
+  }
+];
+
 // Socket listen server
 socketStart = server => io.listen(server);
 
 // Socket connect
 io.on('connection', socket => {
   // socket.handshake.session
-  socket.on('hello', () => console.log('server Hello'));
-  socket.emit('hi');
+  socket.emit('messages', {
+    fakeMessages,
+    login_user: socket.handshake.session.username
+  });
 
   // Socket disconnect
   socket.on('disconnect', () => {
