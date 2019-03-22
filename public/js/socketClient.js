fcc9b6f1b6de664dcfa79578f62369c19344c963
Enes Kahraman <official.phoique@gmail.com>
2019-03-22 16:08:00 +0300

    chat ui bug fix

--- public/js/socketClient.js
+++ public/js/socketClient.js
@@ -5,19 +5,40 @@ socket.on('messages', (data) => {
   fake = data.fakeMessages;
   fake.map(messages => {
 
-    // message html p tag
-    let message_p = document.createElement('p');
-    message_p.className = 'tag is-medium is-warning';
-    message_p.appendChild(document.createTextNode(`${messages.username}: ${messages.message}`));
+    // The user who wrote the message
+    if (messages.username === data.login_user) {
+      
+      // message html p tag
+      let message_p = document.createElement('p');
+      message_p.className = 'tag is-medium is-info';
+      message_p.appendChild(document.createTextNode(messages.message));
 
-    // message posision div
-    let pos_div = document.createElement('div');
-    pos_div.className = 'message-left';
-    pos_div.appendChild(message_p);
+      // message posision div
+      let pos_div = document.createElement('div');
+      pos_div.className = 'message-right';
+      pos_div.appendChild(message_p);
 
-    // Message box append
-    element_id = document.getElementById("message-box");
-    element_id.appendChild(pos_div);
+      // Message box append
+      element_id = document.getElementById("message-box");
+      element_id.appendChild(pos_div);
+
+    } else {
+      
+      // message html p tag
+      let message_p = document.createElement('p');
+      message_p.className = 'tag is-medium is-warning';
+      message_p.appendChild(document.createTextNode(`${messages.username}: ${messages.message}`));
+
+      // message posision div
+      let pos_div = document.createElement('div');
+      pos_div.className = 'message-left';
+      pos_div.appendChild(message_p);
+
+      // Message box append
+      element_id = document.getElementById("message-box");
+      element_id.appendChild(pos_div);
+
+    }
 
   });
   
